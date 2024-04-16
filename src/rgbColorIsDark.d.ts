3389f9e20e5f2c444b069809f4f7500a34b8239d
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-04-16 22:05:14 +0100

    feat: add JSDoc and TS defs

--- src/rgbColorIsDark.d.ts
+++ src/rgbColorIsDark.d.ts
@@ -1,12 +1,6 @@
-// @ts-check
-
-import yiqValue from '@chriscodesthings/rgb-color-yiq-value';
-
 /**
  * Determines if a colour is dark based on the YIQ calculation
  * @param {[number, number, number] | [number, number, number, number]} rgb Array containing red, green, blue and (optional) alpha values
  * @returns {boolean}
  */
-export default function rgbColorIsDark(rgb) {
-    return yiqValue(rgb) < 128;
-}
+export default function rgbColorIsDark(rgb: [number, number, number] | [number, number, number, number]): boolean;
