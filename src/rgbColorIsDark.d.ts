cee3e209573c27731f3fe7f73ad3f0bce7b9a618
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-05-01 17:29:03 +0100

    fix: nicer TS syntax

--- src/rgbColorIsDark.d.ts
+++ src/rgbColorIsDark.d.ts
@@ -1,6 +1,6 @@
 /**
  * Determines if a colour is dark based on the YIQ calculation
- * @param {[number, number, number] | [number, number, number, number]} rgb Array containing red, green, blue and (optional) alpha values
+ * @param {[number, number, number, number|undefined]} rgba Array containing red, green, blue and (optional) alpha values
  * @returns {boolean}
  */
-export default function rgbColorIsDark(rgb: [number, number, number] | [number, number, number, number]): boolean;
+export default function rgbColorIsDark(rgba: [number, number, number, number | undefined]): boolean;
--- src/rgbColorIsDark.js
+++ src/rgbColorIsDark.js
@@ -4,9 +4,9 @@ import yiqValue from '@chriscodesthings/rgb-color-yiq-value';
 
 /**
  * Determines if a colour is dark based on the YIQ calculation
- * @param {[number, number, number] | [number, number, number, number]} rgb Array containing red, green, blue and (optional) alpha values
+ * @param {[number, number, number, number|undefined]} rgba Array containing red, green, blue and (optional) alpha values
  * @returns {boolean}
  */
-export default function rgbColorIsDark(rgb) {
-    return yiqValue(rgb) < 128;
+export default function rgbColorIsDark(rgba) {
+    return yiqValue(rgba) < 128;
 }
